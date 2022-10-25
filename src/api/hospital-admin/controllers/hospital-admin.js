06a2a29d5dd27f7f615256823e06299f2977945b
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-10-26 00:37:40 +0200

    create project backend setup

---
+++ src/api/hospital-admin/controllers/hospital-admin.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * hospital-admin controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::hospital-admin.hospital-admin');

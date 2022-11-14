93c12ea90b71575e24a87cc25a563b330d5c5c4c
MUKANTWARI Françoise <f.mukantwari@alustudent.com>
2022-11-14 20:58:24 +0200

    add documentaion plugin

---
+++ build/email-translation-ms-json.4d4d9150.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkubuzima_privacy=self.webpackChunkubuzima_privacy||[]).push([[6784],{73559:u=>{u.exports={}}}]);
---
+++ build/email-translation-pt-json.292871eb.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkubuzima_privacy=self.webpackChunkubuzima_privacy||[]).push([[3166],{18210:u=>{u.exports={}}}]);
